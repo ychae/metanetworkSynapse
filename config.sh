b2d3716745fc2e8b57065a38527ae6085918d10e
Yooree Chae <ychae@users.noreply.github.com>
2017-03-07 14:41:58 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,10 +4,10 @@
 outputpath="/shared/network/"
 
 # location of Expression data on Synapse
-dataSynId="syn8356371"
+dataSynId="syn7984097"
 
 # id of folder on Synapse that network files will go to
-parentId="syn8360599"
+parentId="syn7984378"
 
 # path to error output
 errorOutput="$outputpath/errorLogs"
@@ -16,4 +16,4 @@ errorOutput="$outputpath/errorLogs"
 outOutput="$outputpath/outLogs"
 
 # commit message
-versionCommitMessage="MCADGS TCX"
+versionCommitMessage="ROSMAP"
