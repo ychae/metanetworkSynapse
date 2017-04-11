be72cd89f24f694e1ddca6a0c3d98ac45b2c1180
Yooree Chae <ychae@users.noreply.github.com>
2017-04-10 18:18:51 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,10 +4,10 @@
 outputpath="/shared/network/"
 
 # location of Expression data on Synapse
-dataSynId="syn8257627"
+dataSynId="syn7984097"
 
 # id of folder on Synapse that network files will go to
-parentId="syn8360593"
+parentId="syn8360591"
 
 # path to error output
 errorOutput="$outputpath/errorLogs"
@@ -16,4 +16,4 @@ errorOutput="$outputpath/errorLogs"
 outOutput="$outputpath/outLogs"
 
 # commit message
-versionCommitMessage="mayo rnaseq tcx modules from reprocessed data"
+versionCommitMessage="rosmap reprocessed rnaseq with no dx adjustment"
