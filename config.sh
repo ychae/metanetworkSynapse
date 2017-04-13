ffa21e439e3c288e85740a2dfff16879e5556a52
Yooree Chae <ychae@users.noreply.github.com>
2017-04-13 15:27:33 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,10 +4,10 @@
 outputpath="/shared/network/"
 
 # location of Expression data on Synapse
-dataSynId="syn8281530"
+dataSynId="syn7984097"
 
 # id of folder on Synapse that network files will go to
-parentId="syn8360596"
+parentId="syn8360591"
 
 # path to error output
 errorOutput="$outputpath/errorLogs"
@@ -16,4 +16,4 @@ errorOutput="$outputpath/errorLogs"
 outOutput="$outputpath/outLogs"
 
 # commit message
-versionCommitMessage="MSSM rnaseq PHG network inference from reprocessed data"
+versionCommitMessage="rosmap reprocessed rnaseq with no dx adjustment"
