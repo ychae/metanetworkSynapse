75df98f4eb3240023f23cfdeaeb9c28ad756d740
Yooree Chae <ychae@users.noreply.github.com>
2017-06-12 16:45:49 -0700

    Update makeModuleSubmission.R

--- makeModuleSubmission.R
+++ makeModuleSubmission.R
@@ -4,22 +4,24 @@
 #bic.net.ids = c(MayoRNAseq.TCX = 'syn8276546', MayoRNAseq.CBE = 'syn8281722', MSBB.IFG = 'syn8349785',
 #                MSBB.PHG = 'syn8345109', MSBB.STG = 'syn8343704', MSBB.FP = 'syn8340017', MayoEGWAS.TCX = 'syn8419174',
 #                MayoEGWAS.CBE = 'syn8421913')
-bic.net.ids = c(ROSMAP = 'syn8268669')
+bic.net.ids = c(MayoRNAseq.CBE = 'syn8281722')
+#bic.net.ids = c(ROSMAP = 'syn8268669')
 
 #rankCons.net.ids = c(MayoRNAseq.TCX = 'syn8276556', MayoRNAseq.CBE = 'syn8281727',  MSBB.IFG = 'syn8349787',
 #                     MSBB.PHG = 'syn8345270', MSBB.STG = 'syn8343716', MSBB.FP = 'syn8340019', MayoEGWAS.TCX = 'syn8419231',
 #                     MayoEGWAS.CBE = 'syn8421921')
-rankCons.net.ids = c(ROSMAP = 'syn8268680')
+rankCons.net.ids = c(MayoRNAseq.CBE = 'syn8281727')
+#rankCons.net.ids = c(ROSMAP = 'syn8268680')
 
 #module.methods = c('CFinder', 'GANXiS', 'fast_greedy', 'label_prop', 'louvain', 'spinglass', 'walktrap', 'infomap', 'linkcommunities');
 module.methods = c('linkcommunities');
-#module.exec.paths = c('/shared/metanetwork/CFinder-2.0.6--1448/', '/shared/metanetwork/GANXiS_v3.0.2/', replicate(7, './'));
+#module.exec.paths = c('/shared/CFinder-2.0.6--1448/', '/shared/GANXiS_v3.0.2/', replicate(7, './'));
 module.exec.paths = './';
-run.id = seq(1,20,1);
+run.id = seq(1,100,1);
 
 repository.name = 'Sage-Bionetworks/metanetworkSynapse'
 branch.name = 'master'
-file.name = 'buildModules.R'  
+file.name = 'buildModules.R'
 
 synapse.config.path = '/shared/apiKey.txt'
 r.library.path = '/shared/rlibs'
@@ -32,6 +34,7 @@ system('mkdir ./submission.scripts')
 fp = file('./submission.modules.sh', open = 'w+')
 writeLines('#!/bin/bash', con = fp, sep = '\n')
 
+
 # Create submission scripts for each network each method
 objs = mapply(function(bicId, rankId, con, runId, modMethods, moduleExecPaths, repositoryName,
                        branchName, fileName, synapseConfigPath, rLibPath){
@@ -40,10 +43,10 @@ objs = mapply(function(bicId, rankId, con, runId, modMethods, moduleExecPaths, r
                           fileName, synapseConfigPath, rLibPath,i){
       fp1 = file(paste0('./submission.scripts/', bicId, '.', rankId, '.', modMethod, '.', i, '.sh'), open = 'w+')
       writeLines('#!/bin/bash', con = fp1, sep = '\n')
-      writeLines(paste('Rscript -e "source(\'buildModules.R\')"', bicId,  rankId, modMethod, moduleExecPath, i, 
+      writeLines(paste('Rscript -e "source(\'buildModules.R\')"', bicId,  rankId, modMethod, moduleExecPath, i,
                        repositoryName, branchName, fileName, synapseConfigPath, rLibPath), con = fp1, sep = '\n')
       close(fp1)
-      
+
       writeLines(paste('qsub -cwd -V',
                        '-pe mpi 4',
                        '-o', paste0('./submission.scripts/', bicId, '.', rankId, '.', modMethod, '.', i, '.out'),
