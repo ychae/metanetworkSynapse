0a2ebc77e2ab69f58d72073b5e5b2f4e7c6339b5
Yooree Chae <ychae@users.noreply.github.com>
2017-08-22 15:19:35 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,10 +4,10 @@
 outputpath="/shared/network/"
 
 # location of Expression data on Synapse
-dataSynId="syn8555302"
+dataSynId="syn8545679"
 
 # id of folder on Synapse that network files will go to
-parentId="syn10347352"
+parentId="syn10347355"
 
 # path to error output
 errorOutput="$outputpath/errorLogs"
@@ -16,4 +16,4 @@ errorOutput="$outputpath/errorLogs"
 outOutput="$outputpath/outLogs"
 
 # commit message
-versionCommitMessage="CRANIO original cohort network analysis"
+versionCommitMessage="CRANIO validation cohort network analysis"
